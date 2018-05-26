7a28456f3d9e03bb642321e9a41ba1896e02d539
Fanny <fannybecerravera@gmail.com>
2018-05-25 22:12:19 -0400

    Agregando segunda condicionante

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,6 @@ let age = prompt("¿Cuántos años tienes?");
 
 if(age <= 3 ) {
     alert("¡You are at the stage toddler!");
-}
+}else if (age <= 5){
+    alert("¡You are at the stage preschooler!");
+
