e56058405b3db06e693230bd5e9992508b01e52a
Fanny <fannybecerravera@gmail.com>
2018-05-25 22:14:33 -0400

    Agregando tercera condicion

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,7 @@ if(age <= 3 ) {
     alert("¡You are at the stage toddler!");
 }else if (age <= 5){
     alert("¡You are at the stage preschooler!");
-
+}else if (age <= 12){
+    alert("¡You are at the stage gradeschooler!");
+}else if (age <= 17){
+    alert("¡You are at the stage teenager!"); 
