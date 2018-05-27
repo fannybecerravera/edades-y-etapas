7b6123ceb6eccb145a96a24077b65911dbce8b3d
Fanny <fannybecerravera@gmail.com>
2018-05-26 22:41:04 -0400

    Agregando comentario

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
+/*EDADES Y ETAPAS
+Crea una web que pida, por medio de un prompt(), la edad de una persona, en años.
+Dependiendo del número brindado, la web debe dar un mensaje que clasifica a la persona en: toddler, 
+preschooler, gradeschooler, teenager, young adult and adult, según el criterio de esta página.
+Si la persona ingresa un dato que no es un número entero (por ejemplo: un string, un número decimal,
+un cero, o un campo vacío), la web debe dar un mensaje de error. 
+Revisa este link que tiene un par de tips interesantes relacionados a este proyecto. */
+
 let age = prompt("¿Cuántos años tienes?");
 
 if(age <= 3 ) {
@@ -13,3 +21,4 @@ if(age <= 3 ) {
 }else {
     alert("¡You are at the stage adult!");
 }
+
